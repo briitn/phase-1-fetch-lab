32ff623c0ae4b2a9eeeb7c2d2ed00b56cc2df5f0
princegobah <babynamek@gmail.com>
2021-12-26 21:27:10 -0500

    commit tree b39cc1cd510c68b4dc01256709dcf1648633e4ab

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res=>res.json())
+  .then(callB=>renderBooks(callB))
+
+ 
 }
 
 function renderBooks(books) {
